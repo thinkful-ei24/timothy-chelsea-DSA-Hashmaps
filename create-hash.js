4fc8822538f34f4d6221f6e759abf635838482d3
clkent <chelsea.kent@gmail.com>
2018-11-12 09:06:09 -0800

    set up hash map and created lor hash

---
+++ create-hash.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const HashMap = require('./hashmap');
+
+let lorArr = [
+  { Hobbit: 'Bilbo' },
+  { Hobbit: 'Frodo' },
+  { Wizard: 'Gandolf' },
+  { Human: 'Aragon' },
+  { Elf: 'Legolas' },
+  { Maiar: 'The Necromancer' },
+  { Maiar: 'Sauron' },
+  { RingBearer: 'Gollum' },
+  { LadyOfLight: 'Galadriel' },
+  { HalfElven: 'Arwen' },
+  { Ent: 'Treebeard' }
+];
+
+function main() {
+  let lor = new HashMap();
+  lorArr.forEach(obj => {
+    Object.keys(obj).forEach(key => lor.set(key, obj[key]));
+  });
+  console.log(lor);
+}
+
+main();
---
+++ hashmap.js
@@ -0,0 +1,93 @@
+'use strict';
+
+class HashMap {
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  //static hash string
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
+
+  //get item
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error('Key error');
+    }
+    return this._slots[index].value;
+  }
+
+  //Adding items
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+
+    const index = this._findSlot(key);
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+    this.length++;
+  }
+
+  //remove item
+  remove(key) {
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  //find available slot
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    //open addressing solution - if slot has value moves to next available
+    for (let i = start; i < start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || (slot.key == key && !slot.deleted)) {
+        return index;
+      }
+    }
+  }
+
+  //resizing hash map
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+    //Reset the length
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
+
+    for (let slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+}
+
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
+
+module.exports = HashMap;
